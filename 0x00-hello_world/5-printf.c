3e289fa96c645170affaf434850fbd48abac4b2d
Youtee-Oton <uteegenius@gmail.com>
2023-02-16 12:04:57 -0800

    Programming is fun

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - to print a code using printf on a new line
+ *
+ * Return: (0)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
