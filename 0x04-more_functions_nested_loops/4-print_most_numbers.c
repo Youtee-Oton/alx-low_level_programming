c14e36e51aabf8caf065fb806cb41bbe75120934
Youtee-oton <uteegenius@gmail.com>
2023-02-24 00:42:37 -0800

    nested

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints
-* owned by Bwave ICT
-*Return: void
+* print_most_numbers - prints
+* Return: void
 */
 
 void print_most_numbers(void)
@@ -12,9 +11,8 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (!(c == '2' || c == '4'))
+		_putchar(c);
 	}
 	_putchar('\n');
 }
-
