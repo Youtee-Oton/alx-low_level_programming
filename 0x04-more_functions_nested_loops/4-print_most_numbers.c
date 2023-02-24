6b1fc89614032686ede895c0089424a4639d696c
Youtee-oton <uteegenius@gmail.com>
2023-02-24 00:52:50 -0800

    nested

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints
-* Return: void
-*/
+ * print_numbers - print 0-9
+ * return: void
+ */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
 		_putchar(c);
 	}
 	_putchar('\n');
