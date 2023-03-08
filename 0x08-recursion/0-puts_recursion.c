f9eddc85ea8afc552295a0545a77a439d79a661f
Youtee-oton <uteegenius@gmail.com>
2023-03-08 12:12:15 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,17 @@
  *  * _puts_recursion - print a string
  *   * @s:string
  *    * Return:void
- *     */
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
-	{
-			_putchar(*s);
-				_puts_recursion(s + 1);
-	}
+{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+}
 	else
-	{
-			_putchar('\n');
-	}
+{
+		_putchar('\n');
+}
 
 }
