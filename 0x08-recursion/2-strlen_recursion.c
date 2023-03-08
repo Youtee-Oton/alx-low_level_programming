b35bae85a3253c9d84204c99738567293c7aa578
Youtee-oton <uteegenius@gmail.com>
2023-03-08 11:59:03 -0800

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,16 +3,15 @@
  *  * _strlen_recursion - length of string
  *   * @s:string
  *    * Return:int
- *     */
+ */
 int _strlen_recursion(char *s)
 {
-		int i = 0;
+	int i = 0;
 
-			if (*s)
-					{
-								i++;
-										i += _strlen_recursion(s + 1);
-
-											}
-				return (i);
+		if (*s)
+	{
+			i++;
+			i += _strlen_recursion(s + 1);
+	}
+	return (i);
 }
