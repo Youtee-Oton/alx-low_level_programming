38a95c8631fc3cf07fa15b912391f9b291190d98
Youtee-oton <uteegenius@gmail.com>
2023-04-12 07:01:51 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1) 
+	if (fd == -1)
 	{
 		free(buffer);
 		return (0);
@@ -34,7 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-       	{
+	{
 		free(buffer);
 		close(fd);
 		return (0);
@@ -42,7 +42,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer[bytes_read] = '\0';
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read) 
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		free(buffer);
 		close(fd);
